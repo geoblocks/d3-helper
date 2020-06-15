e59569d481aaaa5f62c0756d986f43b1798dcbc8
Benjamin Gerber <benjamin.gerber@camptocamp.com>
2020-06-15 10:04:51 +0200

    Test and correct utils

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -2,6 +2,9 @@ import { getRoundedNumber } from './utils';
 
 describe('Utils functions', () => {
   it('getRoundedNumber', () => {
+    expect(getRoundedNumber(null)).toEqual(null);
+    expect(getRoundedNumber(undefined)).toEqual(undefined);
+    expect(getRoundedNumber(NaN)).toEqual(NaN);
     expect(getRoundedNumber(20.25)).toEqual(20);
     expect(getRoundedNumber('20.25')).toEqual(20);
     expect(getRoundedNumber('round a string')).toEqual('round a string');
--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,14 @@
  * @returns value rounded to the nearest integer.
  */
 export function getRoundedNumber(value: number|string): number|string {
+  if (value === null || value === undefined || Number.isNaN(value as number)) {
+    return value;
+  }
+  // If the value contains any not numeric value (except point), return it as string.
+  const stringValue = `${value}`;
+  if (!(/^[\d|\.]+$/.test(stringValue))) {
+    return `${stringValue}`;
+  }
   const floatValue = typeof value === 'string' ? parseFloat(value) : value;
-  return isNaN(floatValue) ? value : Math.round(floatValue);
+  return Math.round(floatValue);
 }
